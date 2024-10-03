ee67571d874fd31eb05ba18fd7fbb8c2a2de40c8
saurabhanand96310 <122476256+saurabhanand96310@users.noreply.github.com>
2024-10-03 21:45:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const inputName=document.getElementById("name");
+const inputYear=document.getElementById("year");
+const h3Content=document.getElementById("url");
+const btn=document.getElementById("button");
+btn.addEventListener('click',(e)=>{
+	e.preventDefault();
+	if(inputName.value!=="" && inputYear.value!=="" ){
+		h3Content.innerText+=`?name=${inputName.value}&year=${inputYear.value}`
+	}
+	else if(inputName.value!==""){
+		h3Content.innerText+=`?name=${inputName.value}`
+	}else if(inputYear.value!==""){
+		h3Content.innerText+=`?year=${inputYear.value}`
+	}
+})
